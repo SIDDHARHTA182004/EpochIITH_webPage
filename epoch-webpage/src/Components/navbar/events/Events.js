9a8602853788b80606fb4c234bb57ea7dc649114
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-12-08 15:30:48 +0530

    events page ready

--- epoch-webpage/src/Components/navbar/events/Events.js
+++ epoch-webpage/src/Components/navbar/events/Events.js
@@ -1,9 +1,54 @@
 import React from 'react'
-
+import './events.css'
 function Events() {
   return (
-    <div style={{background : "none", position : "absolute", color : "white"}}>
-      this is event page
+    <div className='body'>
+      <div className='heading'>
+        Our <span id='colorGreen'>Events</span>
+      </div>
+      <div className='cardHolder'>
+        <a href='https://www.youtube.com' rel="noreferrer" target='_blank'>
+
+        <div className='card'>
+          <div className='image'><img src='https://cdn2.vectorstock.com/i/1000x1000/88/26/no-image-available-icon-flat-vector-25898826.jpg' className='cardImage' alt=""></img></div>
+          <div className='cardContent'>
+            <b style={{background : 'none'}}>Description</b>: Lorem ad nisi et dolore eiusmod proident eiusmod esse nostrud non eiusmod adipisicing. Eiusmod nulla proident velit excepteur magna proident deserunt dolore proident cillum. Consectetur ea magna enim sunt officia id voluptate incididunt esse quis ex fugiat. Aute nulla tempor sint enim commodo dolor ut.<br/>
+            <b style={{background : 'none'}}>year</b> : 2020
+          </div>
+        </div>
+        </a>
+        <a href='https://www.youtube.com' rel="noreferrer" target='_blank'>
+
+        <div className='card'>
+          <div className='image'><img src='https://cdn2.vectorstock.com/i/1000x1000/88/26/no-image-available-icon-flat-vector-25898826.jpg' className='cardImage' alt=""></img></div>
+          <div className='cardContent'>
+            <b style={{background : 'none'}}>Description</b>: Lorem ad nisi et dolore eiusmod proident eiusmod esse nostrud non eiusmod adipisicing. Eiusmod nulla proident velit excepteur magna proident deserunt dolore proident cillum. Consectetur ea magna enim sunt officia id voluptate incididunt esse quis ex fugiat. Aute nulla tempor sint enim commodo dolor ut.<br/>
+            <b style={{background : 'none'}}>year</b> : 2020
+          </div>
+        </div>
+        </a>
+        <a href='https://www.youtube.com' rel="noreferrer" target='_blank'>
+
+        <div className='card'>
+          <div className='image'><img src='https://cdn2.vectorstock.com/i/1000x1000/88/26/no-image-available-icon-flat-vector-25898826.jpg' className='cardImage' alt=""></img></div>
+          <div className='cardContent'>
+            <b style={{background : 'none'}}>Description</b>: Lorem ad nisi et dolore eiusmod proident eiusmod esse nostrud non eiusmod adipisicing. Eiusmod nulla proident velit excepteur magna proident deserunt dolore proident cillum. Consectetur ea magna enim sunt officia id voluptate incididunt esse quis ex fugiat. Aute nulla tempor sint enim commodo dolor ut.<br/>
+            <b style={{background : 'none'}}>year</b> : 2020
+          </div>
+        </div>
+        </a>
+        <a href='https://www.youtube.com' rel="noreferrer" target='_blank'>
+
+        <div className='card'>
+          <div className='image'><img src='https://cdn2.vectorstock.com/i/1000x1000/88/26/no-image-available-icon-flat-vector-25898826.jpg' className='cardImage' alt=""></img></div>
+          <div className='cardContent'>
+            <b style={{background : 'none'}}>Description</b>: Lorem ad nisi et dolore eiusmod proident eiusmod esse nostrud non eiusmod adipisicing. Eiusmod nulla proident velit excepteur magna proident deserunt dolore proident cillum. Consectetur ea magna enim sunt officia id voluptate incididunt esse quis ex fugiat. Aute nulla tempor sint enim commodo dolor ut.<br/>
+            <b style={{background : 'none'}}>year</b> : 2020
+          </div>
+        </div>
+        </a>
+        
+      </div>
     </div>
   )
 }
